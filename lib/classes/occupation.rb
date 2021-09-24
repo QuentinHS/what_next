2496be243b5810aa9893978bee343f848234bfc4
QuentinHS <quentin.halysummerfield@gmail.com>
2021-09-25 01:38:11 +1000

    added mobile footer fixes

--- lib/classes/occupation.rb
+++ lib/classes/occupation.rb
@@ -1,9 +1,15 @@
 class Occupation 
-  def initialize(job_name, salary_range, growing, is_outdoors, myer_briggs_suitability)
+  def initialize(job_name, salary_range, growing, is_primarily_autonomous, is_primarily_outdoors, education_level_required,involves_social_responsibility, involves_helping_others, has_significant_variety, myer_briggs_suitability)
     @job_name = job_name
     @salary_range = salary_range
     @growing = growing
-    @is_outdoors = is_outdoors
+    @is_primarily_automous? = is_primarily_autonomous
+    @is_primarily_outdoors? = is_primarily_outdoors
+    @education_level_required = education_level_required 
+    @involves_social_responsibility = involves_social_responsibility 
+    @involves_helping_others = involves_helping_others 
+    @has_significant_variety = has_significant_variety
     @myer_briggs_suitability = myer_briggs_suitability
+    
   end
 end
--- lib/classes/user.rb
+++ lib/classes/user.rb
@@ -1,7 +1,8 @@
 class User
-  def initialize(name, favorite_school_subject, desired_salary_range)
+  def initialize(name, favorite_school_subject, enjoy_formal_education, desired_salary_range)
     @name = name
-    @favorite_school_subject = favorite_school_subject 
+    @favorite_school_subject = favorite_school_subject
+    @enjoy_formal_education = enjoy_formal_education
     @desired_salary_range = desired_salary_range
     @myer_briggs_type = nil
   end
