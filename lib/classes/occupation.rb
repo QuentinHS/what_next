7ea9788e84704642d1a1c1ce19033d8dbd3d73f2
QuentinHS <quentin.halysummerfield@gmail.com>
2021-09-29 02:44:40 +1000

    fixed compare jobs bug

--- lib/classes/occupation.rb
+++ lib/classes/occupation.rb
@@ -7,12 +7,12 @@ occupations = []
 
 class Occupation 
 
-  attr_reader :job_name, :salary_range
+  attr_reader :job_name, :salary_min, :salary_average, :growing, :job_size, :personality_suitability
 
-  def initialize(job_name, salary_min, salary_max, growing, job_size, personality_suitability)
+  def initialize(job_name, salary_min, salary_average, growing, job_size, personality_suitability)
     @job_name = job_name
     @salary_min = salary_min
-    @salary_max = salary_max
+    @salary_average = salary_average
     @growing = growing
     @job_size = job_size
     @personality_suitability = personality_suitability
@@ -26,30 +26,37 @@ class Occupation
   end
   
   # Calculates the size of a profession
-  def calculate_occupation_size
-    self.job_size > 100000 ? "large" : self.job_size  > 50000 ? "medium" : "small" 
-  end
+  # def calculate_occupation_size
+  #   self.job_size > 100000 ? "large" : self.job_size  > 50000 ? "medium" : "small" 
+  # end
 
   # Compare two occupations based on instance attributes
 
-  # def compare_occupation(compared_occupation)
-  #   puts "#{self.job_name} has a #{self.salary_range == compared_occupation.salary_range ? "similar" : } salary range than #{compared_occupation.job_name}"
-  # end
+  def compare_occupation(compared_occupation)
+    puts "#{self.job_name} generally has a #{self.salary_min == compared_occupation.salary_min ? "similar" : self.salary_min > compared_occupation.salary_min ? "higher" : "lower "} minimum salary than #{compared_occupation.job_name}"
+  end
 
 end
 
 
 data.each do |item|
-  occupation = Occupation.new(item[:name], item[:salary_min], item[:growing], item[:job_size], item[:personality_suitability])
+  occupation = Occupation.new(item[:name], item[:salary_min], item[:salary_average], item[:growing], item[:job_size], item[:personality_suitability])
  
   occupations << occupation
 
 end
 
+
+
 # occupations.each do |item|
 #   puts item.job_name
+#   puts item.salary_min
+#   puts item.salary_average
+#   puts item.growing
+#   puts item.job_size
+#   puts item.personality_suitability
 # end
 
-# occupations[0].compare_occupation(occupations[3])
+occupations[3].compare_occupation(occupations[6])
 
  
