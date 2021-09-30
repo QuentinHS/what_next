9fd401db23afb0b47393e69f5ab3eff4c848c3ca
QuentinHS <quentin.halysummerfield@gmail.com>
2021-09-30 15:49:54 +1000

    added personality descriptions

--- lib/modules/temperament_info.rb
+++ lib/modules/temperament_info.rb
@@ -5,9 +5,60 @@ module TemperamentInfo
     puts "It looks like you're an artisan! Artisans have a natural affinity towards the arts, creative and highly-skilled activities.".green
   end
 
+  def self.guardian_info
+    puts "It looks like you're a guardian! Guarians seek security and belonging, and they value duty, loyality, discipline and responsibility. They tend to excel at activities such as logistics, organising, and support.".green
+  end
+
+  def self.idealist_info
+    puts "It looks like you're an idealist! Idealists are compassion and abstract people, who look for the deep meaning of things, and place great value on their personal growth. Finding their own voice is one of the major drives of idealists.".green
+  end
+
+    def self.rational_info
+    puts "It looks like you have a rational personality type! Rational types are independent problem solvers and free thinkers who are able to identify patterns, theories and possibilities that escape many others. They greatest strength is their ability to strategise.".green
+  end
+
   def self.estp_info
     puts "Your personality subtype is the Promoter, a type of Artisan. Promoters like to engage with people, and are persuasive, magnetic and proactive. They are often smooth talkers and risk takers.They tend to be good at reading other people, and good at tactial thinking. They also have an appreciation for the finer things in life.".magenta
   end
+
+    def self.istp_info
+    puts "Your personality subtype is the Crafter, a type of Artisan. Crafters are masters of specialised equipment and tools, and are often drawn to various tools, toys or instruments from a young age. They are oten risk takers and prefer to communicate through actions rather than words. They can also be rebellious and prefer to do things in their own way.".magenta
+  end
+
+   def self.esfp_info
+    puts "Your personality subtype is the Performer, a type of Artisan. Performers are known for their warmth and good-humour, and often possess extraordinary skills in music, comedy or drama. They prefer to be in the company of others, and bring excitement wherever they go. They enjoy being the center of attention, and are incurably optimistic. They are also willing to try almost anything if it will mean a good time, sometimes without regard to the consequences.".magenta
+  end
+
+  def self.isfp_info
+    puts "Your personality subtype is the Composer, a type of Artisan. Composers are highly in tune with their senses, and have an innante sense of what works and what doesn't in art. They are often seized by their aristic motives, and can lose themselves in their pursuits. They are sensitive to the pain and suffering of others, and have a rapport with children and animals.".magenta
+  end
+
+  
+  def self.estj_info
+    puts "Your personality subtype is the Supervisor, a type of Guardian. Supervisors are community-minded individuals who are generous with their time, money and energy, and seek to benefit their community in whatever way they can. Supervisors generally like to take charge and be in control, but their responsibility seriously, and will subordinate themselves and cooperate where necessary. Their greatest virtue is their work ethic, while their weaknesses can be a failure to question authority and tradition.".magenta
+  end
+
+  def self.istj_info
+    puts "Your personality subtype is the Inspector, a type of Guardian. Inspectors are highly dependable individuals who seek to keep an eye on whatever they are responsible for. They are the guardians of society and institutions, and value responsibility above all else, ensuring schedules and standards are maintained. They may be seen as hard-hearted due to their demanding and fastidious nature, yet inspectors do not mind if their work goes unappreciated, as they understand its importance.".magenta
+  end
+
+  def self.esfp_info
+    puts "Your personality subtype is the Provider, a type of Guardian. Providers are the most social type of Guardians, and are highly cooperative and friendly, giving their time, money and energy freely. They make excellent community leaders and hosts or hostesses, as they love to entertain and are concerned about the needs of guests. They are fascinated with local news, and are often the major sources of local gossip. They are deeply loving and affectionate, fear personal criticism, and are happiest when given appreciation for the service they give to others".magenta
+  end
+
+  def self.isfj_info
+    puts "Your personality subtype is the Protector, a type of Guardian. Protectors are primarily interested in the safety and security of those that they care about. They are immensely loyal, and filled with a sense of duty and obligation to their family, friends and community. They dislike constantly changing rules and conditions, and value tradition and heritage. They place value on rank and titles, cherish family history, and work diligently in jobs others may avoid. Their lack of external emotion and shyness may be misinterpreted as coldness, but Protectors are typically sympathetic and warm-hearted to those they know and care about.".magenta
+  end
+
+  def self.isfj_info
+    puts "Your personality subtype is the Teacher, a type of Idealist. Teachers have a natural affinity towards guiding others, with a seemingly limitless ability to come up with imaginative learning activities. They consider other people to be their highest priority, and tend to possess excellent communication skills, as well as being charismatic public speakers. They take commitments seriously, and are highly-organised. Teachers have deep insights into their own nature, but are also sensitive to the feelings and needs of others, and will often pick up emotions and beliefs from those around them.".magenta
+  end
+
+  def self.isfj_info
+    puts "Your personality subtype is the Counselor, a type of Idealist. Counselors are a rare breed indeed, and highly motivated by helping others realise their potential. They are private people with rich inner lives, which may be a puzzle even to themselves. They are concerned with how others feel, and are good at helping organisations run smoothly. They possess vivid imaginations, and their sensitive natures may help them recognise in others things that person cannot see in themselves.".magenta
+  end
+  
+  
   
 
   
